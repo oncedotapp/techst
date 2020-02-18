2aeed7866f1f02766e0a3a2f50f1d8d3ead3e11c
Florian VENTURINI <florian@once.app>
2020-02-18 15:54:45 +0100

    [BACK] add API skeleton

---
+++ src/config/env.ts
@@ -0,0 +1,22 @@
+const requiredEnvVars: string[] = [
+  'NODE_ENV',
+  'SERVER_PORT',
+  'REDIS_HOST',
+  'REDIS_PORT',
+];
+
+/**
+ * Check that all the required variables are set before running the server.
+ */
+export function checkRequiredEnvVars(): void {
+  requiredEnvVars.forEach((envVar: string): void => {
+    if (process.env[envVar] === undefined) {
+      throw new Error(`${envVar} is undefined.`);
+    }
+  });
+}
+
+export const NODE_ENV = (process.env.NODE_ENV as string);
+export const SERVER_PORT = (process.env.SERVER_PORT as string);
+export const REDIS_HOST = (process.env.REDIS_HOST as string);
+export const REDIS_PORT = (process.env.REDIS_PORT as string);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,71 @@
-console.log('COUCOU');
+import 'reflect-metadata'; // So @Inject() works for providers
+
+import { GraphQLModule } from '@graphql-modules/core';
+import { ApolloServer } from 'apollo-server-express';
+import express from 'express';
+
+import {
+  checkRequiredEnvVars,
+  SERVER_PORT,
+} from './config/env';
+
+checkRequiredEnvVars();
+
+/**
+ * Setup Express
+ */
+const app = express();
+
+app.disable('x-powered-by');
+
+/**
+ * HealthCheck route
+ */
+app.get(
+  '/the/check/is/a/lie',
+  (_: express.Request, response: express.Response): express.Response => response.status(200).send('OK'),
+);
+
+/**
+ * Setup ApolloServer
+ */
+
+// Merge required modules into one
+const MergeModule = new GraphQLModule({
+  name: 'TechstMerge',
+  imports: [
+    // AnalyticsModule,
+    // AuthModule,
+    // BlockOptionModule,
+    // CardModule,
+    // FieldModule,
+    // FileModule,
+    // FontModule,
+    // ItemModule,
+    // ProfileModule,
+    // SessionModule,
+    // StoryModule,
+    // TagModule,
+    // ThirdPartyApisModule,
+    // TsvExportModule,
+    // UploadModule,
+    // UserModule,
+    // VoteModule,
+  ],
+});
+
+const { schema } = MergeModule;
+
+const server = new ApolloServer({
+  schema,
+  context: (session): { req: express.Request; res: express.Response } => session, // Do not remove it or providers won't have access to the session.
+});
+
+server.applyMiddleware({
+  app,
+  path: '/',
+});
+
+app.listen(SERVER_PORT, () => {
+  console.log(`🚀 Techst API ready at http://localhost:${SERVER_PORT}`);
+});

23e77c6b3c9fb544f8a97e3fc23e231fc4ecc95a
Florian VENTURINI <florian@once.app>
2020-02-19 15:58:24 +0100

    [FRONT] Remove useless types + fontfaceobserver for OpenSans

--- frontend/app/app.tsx
+++ frontend/app/app.tsx
@@ -1,20 +1,8 @@
-// Import all the third party stuff
 import React from 'react';
 import ReactDOM from 'react-dom';
-import FontFaceObserver from 'fontfaceobserver';
 import 'sanitize.css/sanitize.css';
 
-// Import root app
 import App from './containers/App';
-
-// Observe loading of Open Sans (to remove open sans, remove the <link> tag in
-// the index.html file and this observer)
-const openSansObserver = new FontFaceObserver('Open Sans', {});
-
-// When Open Sans is loaded, add a font-family using Open Sans to the body
-openSansObserver.load().then(() => {
-  document.body.classList.add('fontLoaded');
-});
 
 // Create redux store with history
 const MOUNT_NODE = document.getElementById('app') as HTMLElement;

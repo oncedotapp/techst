c4acc8e62b8ec71edb146f43a76052c3b3b957cf
Florian VENTURINI <florian@once.app>
2020-03-02 14:49:32 +0100

    [FRONT] Upgrade packages + move exercise doc to Readme

--- frontend/app/components/CodeInput/index.tsx
+++ frontend/app/components/CodeInput/index.tsx
@@ -3,18 +3,6 @@ import React from 'react';
 import Form from './styled/form';
 import Input from './styled/input';
 
-/**
- * Welcome to the Techst...
- * Your mission, should you choose to accept it, will be to code the CodeInput component.
- *
- * The CodeInput component is used by the users to enter their multi-digit login code (received by email)
- * Here are some specs about this component: (not in any difficulty order or anything)
- * - Whichever box you click on, your cursor should always be active on the left most box
- * - You can only write up to 1 **digit** per box
- * - Writing a digit in a box should bring your cursor to the next box
- * - Once the code is fully written, the `onCodeFull` callback should be called (with the code as parameter obviously)
- */
-
 export type Props = {
   error?: boolean;
   total: number;

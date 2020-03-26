397fa6f69a5964a2e6b560a8359d26fde91deebf
Florian VENTURINI <florian@once.app>
2020-03-26 15:55:20 +0100

    [FRONT] Add stories for styled components

--- frontend/app/components/CodeInput/index.stories.tsx
+++ frontend/app/components/CodeInput/index.stories.tsx
@@ -6,12 +6,23 @@ import withApollo from '../../utils/withApollo';
 import withFonts from '../../utils/withFonts';
 
 import CodeInput from '.';
+import Form from './styled/form';
+import Input from './styled/input';
 
 export default {
   title: 'CodeInput',
   decorators: [withKnobs, withApollo, withFonts],
 };
 
-export const readyToBeUsed: React.FC = () => (
+export const toDo: React.FC = () => (
   <CodeInput total={number('Code length', 4)} error={false} onCodeFull={action('Code to send')} />
+);
+
+export const input = () => <Input />;
+
+export const formWithInputs = () => (
+  <Form>
+    <Input />
+    <Input />
+  </Form>
 );

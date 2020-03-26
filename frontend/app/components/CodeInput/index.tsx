f948e0ea1211f2ca16a963c6d3a578e3624ff1c3
Florian VENTURINI <florian@once.app>
2020-03-26 15:49:23 +0100

    [FRONT] Shorten the starting code

--- frontend/app/components/CodeInput/index.tsx
+++ frontend/app/components/CodeInput/index.tsx
@@ -1,21 +1,11 @@
 import React from 'react';
 
-import Form from './styled/form';
-import Input from './styled/input';
-
 export type Props = {
   error?: boolean;
   total: number;
   onCodeFull: (code: string) => void;
 };
 
-const CodeInput: React.FC<Props> = ({ total }) => (
-  <Form>
-    {[...Array(total)].map((_, idx: number) => (
-      // eslint-disable-next-line react/no-array-index-key
-      <Input key={idx} />
-    ))}
-  </Form>
-);
+const CodeInput: React.FC<Props> = () => <div>TODO :)</div>;
 
 export default CodeInput;

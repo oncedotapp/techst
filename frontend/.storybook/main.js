792f2ce4af4c986a580759797e0be723a1533caa
Florian VENTURINI <florian@once.app>
2020-10-07 18:47:20 +0200

    [FRONT] Update SB config for 6.x

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -1,18 +1,7 @@
 module.exports = {
   addons: [
-    '@storybook/addon-actions/register',
-    '@storybook/addon-knobs/register',
+    '@storybook/addon-actions',
+    '@storybook/addon-knobs',
   ],
   stories: ['../app/**/*.stories.tsx'],
-  webpackFinal: async config => {
-    config.module.rules.push({
-      test: /\.(ts|tsx)$/,
-      loader: require.resolve('babel-loader'),
-      options: {
-        presets: [['react-app', { flow: false, typescript: true }]],
-      },
-    });
-    config.resolve.extensions.push('.ts', '.tsx');
-    return config;
-  },
 };

88793dd3698c56e69ef923533a967590365a7939
Florian VENTURINI <florian@once.app>
2021-10-26 17:55:38 +0200

    Add font + fix root page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,9 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import { CodeInput } from '../src/CodeInput';
 
-import styles from '../styles/Home.module.css'
-
-export default function Playground() {
+export default function Playground(): JSX.Element {
   return (
-    <div className={styles.container}>
-      We will code our exercise here!
+    <div>
+      <CodeInput totalDigits={6} onCodeFull={console.log} />
     </div>
   );
 }

ac2ea2f31c32850335dbae073c69e6b443c23ef3
Florian VENTURINI <florian@once.app>
2021-12-18 11:49:04 +0100

    Upgrade dependencies

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+
+  // https://github.com/vercel/next.js/issues/26799
+  // https://nextjs.org/docs/advanced-features/compiler#styled-components
+  experimental: {
+    styledComponents: true,
+  },
 }
